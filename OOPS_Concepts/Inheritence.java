53156f2f5c8bfbf45436dccf21f103e02d639b81
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-12-29 20:57:09 +0530

    Inheritance- in Java

--- OOPS_Concepts/Inheritence.java
+++ OOPS_Concepts/Inheritence.java
@@ -6,7 +6,7 @@ public class Inheritence {
         // derived from parent class.
         Dog d = new Dog();
         d.eat();
-        d.run();
+        // d.run();
         d.barks();
     }
 }
@@ -30,13 +30,52 @@ class Divya extends Student {
 }
 
 // multi-level inheritance
-class Animal {
-    void eat() {
-        System.out.println("eating...");
-    }
+// class Animal {
+// void eat() {
+// System.out.println("eating...");
+// }
 
-    void run() {
-        System.out.println("running...");
+// void run() {
+// System.out.println("running...");
+// }
+// }
+
+// class Mammel extends Animal {
+// void walks() {
+// System.out.println("walking...");
+// }
+// }
+
+// class Dog extends Mammel {
+// void barks() {
+// System.out.println("barking...");
+// }
+// }
+
+// hierarchical inheritance
+// class OtherAnimal {
+// int numberOfAnimal;
+// }
+
+// class Fish extends OtherAnimal {
+// void swim() {
+// System.out.println("swimming...");
+// }
+// }
+
+// class lion extends OtherAnimal {
+// void roar() {
+// System.out.println("roaring...");
+// }
+// }
+
+// Hybrid Inheritance
+class Animal {
+    String name;
+    int age;
+
+    void eat() {
+        System.out.println(name + " is eating.");
     }
 }
 
@@ -52,19 +91,38 @@ class Dog extends Mammel {
     }
 }
 
-// hierarchical inheritance
-class OtherAnimal {
-    int numberOfAnimal;
-}
-
-class Fish extends OtherAnimal {
-    void swim() {
-        System.out.println("swimming...");
+class Car extends Mammel {
+    void run() {
+        System.out.println("running...");
     }
 }
 
-class lion extends OtherAnimal {
-    void roar() {
-        System.out.println("roaring...");
+class Fish extends Animal {
+    void breatheWater() {
+        System.out.println("Fish can Breath Water!");
+    }
+}
+
+class Shark extends Fish {
+    void breatheAir() {
+        System.out.println("Shark can't Breath Air!");
+    }
+}
+
+class whale extends Fish {
+    void breatheOxygen() {
+        System.out.println("Whale can Breath Oxygen!");
+    }
+}
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying.");
+    }
+}
+
+class Peacock extends Bird {
+    void showTail() {
+        System.out.println("My beautiful tail is showing.");
     }
 }
