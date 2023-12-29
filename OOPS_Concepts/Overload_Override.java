90a507fd4485701b9b058417aacee00793ca158e
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-12-29 23:54:20 +0530

    Overloading and Overriding in Java

--- OOPS_Concepts/Overload_Override.java
+++ OOPS_Concepts/Overload_Override.java
@@ -3,12 +3,15 @@ package OOPS_Concepts;
 
 public class Overload_Override {
     public static void main(String[] args) {
-        Calc newCalc = new Calc(); // Method Overloading
-        System.out.println(newCalc.sum(5, 7));
-        System.out.println(newCalc.sum(8, 9, 10));
+        // Calc newCalc = new Calc(); // Method Overloading
+        // System.out.println(newCalc.sum(5, 7));
+        // System.out.println(newCalc.sum(8, 9, 10));
+        Dog d = new Dog(); // Method Overriding
+        d.runs();
     }
 }
 
+// Method Overloading
 class Calc {
     int sum(int a, int b) {
         return a + b;
@@ -16,5 +19,18 @@ class Calc {
 
     int sum(int a, int b, int c) {
         return a + b + c;
+    }
+}
+
+// Method Overriding
+class Animal {
+    void runs() {
+        System.out.println("running...");
+    }
+}
+
+class Dog extends Animal {
+    void runs() {
+        System.out.println("runs fast...");
     }
 }
