3573e4044af963d4d001c0810728a98de6c0eed3
Divyaa Sharmaa <shdivya1310@gmail.com>
2024-01-13 23:13:41 +0530

    Merge Sort

---
+++ DivideAndConquer/mergeSorting.java
@@ -0,0 +1,59 @@
+package DivideAndConquer;
+
+public class mergeSorting {
+
+    public static void main(String[] args) {
+    int arr[] = {4,2,9,5,3,1};
+    int si = 0;
+    int ei = arr.length-1;
+    mergeSort(arr, si,ei);
+    printArr(arr);
+    }
+
+    public static void mergeSort(int[] arr, int si, int ei){
+        int mid = si + (ei-si)/2;
+        if(si>=ei){
+            return;
+        }
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid+1, ei);
+        merge(arr, si, mid, ei);
+    }
+    
+    public static void merge(int arr[], int si, int mid, int ei){
+        int[] temp = new int[ei-si+1];
+        int i = si;
+        int j = mid+1;
+        int k = 0;
+
+        while(i<=mid && j<=ei){
+            if(arr[i] <= arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+
+        while(j<=ei){
+            temp[k++] = arr[j++];
+        }
+
+        //copy temp to arr
+        for(k=0,i=si;k<temp.length;k++,i++){
+            arr[i]=temp[k];
+        }
+    }
+
+    public static void printArr(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
