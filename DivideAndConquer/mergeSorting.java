ad465e2ca40a7d46201971ca65eed1d3c7d7c83b
Divyaa Sharmaa <shdivya1310@gmail.com>
2024-01-13 23:24:51 +0530

    Merge Sort

--- DivideAndConquer/mergeSorting.java
+++ DivideAndConquer/mergeSorting.java
@@ -3,55 +3,55 @@ package DivideAndConquer;
 public class mergeSorting {
 
     public static void main(String[] args) {
-    int arr[] = {4,2,9,5,3,1};
-    int si = 0;
-    int ei = arr.length-1;
-    mergeSort(arr, si,ei);
-    printArr(arr);
+        int arr[] = { 4, 2, 9, 5, 3, 1, -2 };
+        int si = 0;
+        int ei = arr.length - 1;
+        mergeSort(arr, si, ei);
+        printArr(arr);
     }
 
-    public static void mergeSort(int[] arr, int si, int ei){
-        int mid = si + (ei-si)/2;
-        if(si>=ei){
+    public static void mergeSort(int[] arr, int si, int ei) {
+        int mid = si + (ei - si) / 2;
+        if (si >= ei) {
             return;
         }
         mergeSort(arr, si, mid);
-        mergeSort(arr, mid+1, ei);
+        mergeSort(arr, mid + 1, ei);
         merge(arr, si, mid, ei);
     }
-    
-    public static void merge(int arr[], int si, int mid, int ei){
-        int[] temp = new int[ei-si+1];
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int[] temp = new int[ei - si + 1];
         int i = si;
-        int j = mid+1;
+        int j = mid + 1;
         int k = 0;
 
-        while(i<=mid && j<=ei){
-            if(arr[i] <= arr[j]){
+        while (i <= mid && j <= ei) {
+            if (arr[i] <= arr[j]) {
                 temp[k] = arr[i];
                 i++;
-            }else{
+            } else {
                 temp[k] = arr[j];
                 j++;
             }
             k++;
         }
-        while(i<=mid){
+        while (i <= mid) {
             temp[k++] = arr[i++];
         }
 
-        while(j<=ei){
+        while (j <= ei) {
             temp[k++] = arr[j++];
         }
 
-        //copy temp to arr
-        for(k=0,i=si;k<temp.length;k++,i++){
-            arr[i]=temp[k];
+        // copy temp to arr
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
         }
     }
 
-    public static void printArr(int[] arr){
-        for(int i=0;i<arr.length;i++){
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
